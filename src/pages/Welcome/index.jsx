b64488e0e939f0ac2ad9397f1bf518a0a1b6acb2
JYROH <ghgt1@naver.com>
2023-01-25 14:13:29 +0900

    feat: Add Welcome Page Component

--- src/pages/Welcome/index.jsx
+++ src/pages/Welcome/index.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import styles from "./Welcome.module.scss";
+import Button from "../../components/UI/Button";
 
 const Welcome = () => {
-  return <div>Welcome</div>;
+  return (
+    <div className={styles.welcome}>
+      <p className={styles.phrase}>
+        <i class="fa-solid fa-music"></i>
+        <span>MUQUIZZ</span>
+      </p>
+      <p>음악의 신은 누구?</p>
+      <p>여러 플레이어들과 당신의 음악 실력을 겨뤄보세요!</p>
+      <Button>회원가입</Button>
+    </div>
+  );
 };
 
 export default Welcome;
