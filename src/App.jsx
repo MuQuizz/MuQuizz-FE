b1ff67716c94557b52741d2f44414e00537d4283
JYROH <ghgt1@naver.com>
2023-01-28 00:48:35 +0900

    docs: Add comments

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,10 @@ function App() {
     if (!login) navigate("/");
   }, [login]);
 
+  // 나중에 내용추가.
+  // 로그인이 된 상태면 아바타 상태를 redux-persist에 미리 저장해놔야함
+  // api로 아바타 정보받아온것을 넣어야한다 이말임.
+
   return (
     <div className="App">
       <main>
