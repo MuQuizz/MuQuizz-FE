f98529c5f60cb0f69f2c137762c2073febe8f92c
JYROH <ghgt1@naver.com>
2023-02-03 13:06:35 +0900

    feat: Add React-query package

--- src/main.jsx
+++ src/main.jsx
@@ -14,8 +14,11 @@ import { Provider } from "react-redux";
 import store from "./store/store";
 import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const persistor = persistStore(store);
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
@@ -42,9 +45,11 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <RouterProvider router={router} />
-    </PersistGate>
-  </Provider>
+  <QueryClientProvider client={queryClient}>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={router} />
+      </PersistGate>
+    </Provider>
+  </QueryClientProvider>
 );
