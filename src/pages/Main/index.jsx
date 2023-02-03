0a895766a3795cc9b9a42a44b1bd4e8b0795bba2
JYROH <ghgt1@naver.com>
2023-02-03 16:30:13 +0900

    feat: Fetch Data by React-query with interval 1000ms

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -4,6 +4,7 @@ import MainContainer from "../../components/UI/MainContainer";
 import Description from "../../components/Main/Description";
 import RoomList from "../../components/Main/RoomList";
 import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 const Main = () => {
   // 여기서 데이터를 받아와서 props로 내려줄거임
@@ -13,29 +14,41 @@ const Main = () => {
   // 방목록들 같은 경우는 실시간 동기화가 필요함.
   // 따라서 react query를 사용해보자
 
-  const resultData = [
-    {
-      roomId: 1,
-      status: "ready",
-      roomName: "노쳐맞 할사람 모두 모여라~",
-      maxNumOfPeople: 8,
-      currNumOfPeople: 1,
-    },
-    {
-      roomId: 2,
-      status: "ready",
-      roomName: "초보만 오세요 초보만",
-      maxNumOfPeople: 8,
-      currNumOfPeople: 1,
-    },
-    {
-      roomId: 3,
-      status: "ready",
-      roomName: "개 초보요",
-      maxNumOfPeople: 8,
-      currNumOfPeople: 1,
-    },
-  ];
+  const fetchResultData = useQuery(
+    ["rooms"],
+    () =>
+      axios.get("http://52.79.68.113:8080/room/list").then((a) => {
+        console.log(a);
+        return a.data;
+      }),
+    { refetchInterval: 1000 }
+  );
+
+  const resultData = fetchResultData.data;
+
+  // const resultData = [
+  //   {
+  //     roomId: 1,
+  //     status: "ready",
+  //     roomName: "노쳐맞 할사람 모두 모여라~",
+  //     maxNumOfPeople: 8,
+  //     currNumOfPeople: 1,
+  //   },
+  //   {
+  //     roomId: 2,
+  //     status: "ready",
+  //     roomName: "초보만 오세요 초보만",
+  //     maxNumOfPeople: 8,
+  //     currNumOfPeople: 1,
+  //   },
+  //   {
+  //     roomId: 3,
+  //     status: "ready",
+  //     roomName: "개 초보요",
+  //     maxNumOfPeople: 8,
+  //     currNumOfPeople: 1,
+  //   },
+  // ];
 
   // 현재 클릭한 방에 따라서 descriptionData가 바뀌어야함. 이건 state로 하자.
 
