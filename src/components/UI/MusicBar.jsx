501baddf46b1327ea7a4b86627a74534842a0f06
JYROH <ghgt1@naver.com>
2023-02-03 13:52:40 +0900

    feat: Create Music Bar Css Component

---
+++ src/components/UI/MusicBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./MusicBar.module.scss";
+
+const MusicBar = () => {
+  return (
+    <div className={styles.bar}>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
+};
+
+export default MusicBar;
