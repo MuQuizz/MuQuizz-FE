b4ef78b56baa5e537a84b1a86772f576ea31ba6c
JYROH <ghgt1@naver.com>
2023-02-03 14:13:31 +0900

    feat: Add dynamic changing Description feature

--- src/components/Main/Description.jsx
+++ src/components/Main/Description.jsx
@@ -4,17 +4,22 @@ import MusicBar from "../UI/MusicBar";
 
 const Description = ({ data }) => {
   const status = data.status === "ready" ? "준비 중" : "게임 중";
-  return (
-    <div className={styles.container}>
-      <p>{`${data.roomId}번 방`}</p>
-      <h1>{data.roomName}</h1>
-      <MusicBar />
-      <div className={styles.description}>
-        <p>{`정원: 1/${data.maxNumOfPeople}`}</p>
-        <p>{`상태: ${status}`}</p>
+  console.log(data);
+  const descriptionRes =
+    data.roomId === "" ? (
+      <p className={styles.select}>방을 선택해주세요.</p>
+    ) : (
+      <div className={styles.container}>
+        <p>{`${data.roomId}번 방`}</p>
+        <h1>{data.roomName}</h1>
+        <MusicBar />
+        <div className={styles.description}>
+          <p>{`정원: ${data.currNumOfPeople}/${data.maxNumOfPeople}`}</p>
+          <p>{`상태: ${status}`}</p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  return descriptionRes;
 };
 
 export default Description;
